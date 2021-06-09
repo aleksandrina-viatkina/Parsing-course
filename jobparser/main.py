028f94d2d68994e4d324c9d176fa23e929225496
Aleksandrina <avyatkina.spb@gmail.com>
2021-06-09 15:37:44 +0300

    les5_hw

---
+++ jobparser/main.py
@@ -0,0 +1,11 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from jobparser.spiders.hhru import HhruSpider
+
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule('jobparser.settings')
+    crawler_process = CrawlerProcess(settings=crawler_settings)
+    crawler_process.crawl(HhruSpider)
+    crawler_process.start()
